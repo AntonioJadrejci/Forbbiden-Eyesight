00cc03513c3f5f6d6a4a4379454dad15ae45c8e1
Antonio <tonisajbic501@gmail.com>
2024-05-29 18:52:18 +0200

    Door changes
    Changed the door mechanics - now opens properly

--- Assets/DoorController.cs
+++ Assets/DoorController.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     public string doorName = "Door_2";
-    public float openAngle = 90f;
-    public float closedAngle = 0f;
-    public float smooth = 2f;
+    public float openAngle = 180f;
+    public Vector3 closedRotation = new Vector3(-94.388f, -12.919f, -267.925f);
+    public float smooth = 5f;
     private bool isOpen = false;
     private Quaternion targetRotation;
 
     void Start()
     {
-        targetRotation = Quaternion.Euler(0, closedAngle, 0);
+        targetRotation = Quaternion.Euler(closedRotation);
+        transform.rotation = targetRotation; // osiguraè da su vrata u poèetnoj pozicji
+        Debug.Log("Initial rotation set to closed position: " + closedRotation);
     }
 
     void Update()
@@ -23,14 +25,30 @@ public class DoorController : MonoBehaviour
     {
         isOpen = !isOpen;
 
-        targetRotation = isOpen ? Quaternion.Euler(0, openAngle, 0) : Quaternion.Euler(0, closedAngle, 0);
+        if (isOpen)
+        {
+            // 
+            targetRotation = Quaternion.Euler(closedRotation + new Vector3(0, openAngle, 0));
+            Debug.Log("Opening door to rotation: " + (closedRotation + new Vector3(0, openAngle, 0)));
+        }
+        else
+        {
+            // reset na zatvorena
+            targetRotation = Quaternion.Euler(closedRotation);
+            Debug.Log("Closing door to rotation: " + closedRotation);
+        }
     }
 
     void OnMouseDown()
     {
         if (gameObject.name == doorName)
         {
+            Debug.Log("Door clicked: " + doorName);
             ToggleDoor();
+        }
+        else
+        {
+            Debug.Log("Clicked object is not the door: " + gameObject.name);
         }
     }
 }
