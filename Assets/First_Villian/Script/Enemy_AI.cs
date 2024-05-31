3f2eed02b858305249d54896864b7015e1bb86ec
gregorkostic <gregor.kostic11@gmail.com>
2024-05-31 12:18:57 +0200

    mini fix

--- Assets/First_Villian/Script/Enemy_AI.cs
+++ Assets/First_Villian/Script/Enemy_AI.cs
@@ -3,161 +3,140 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy_AI : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
-    public GameObject Lucas;
-    public NavMeshAgent agent;
+    public GameObject Lucas; // Target player
+    public NavMeshAgent agent; // Navigation agent
 
-    [SerializeField] LayerMask groundLayer, LucasLayer;
+    [SerializeField] LayerMask whatIsGround, whatIsPlayer; // Detection layers
 
-    Animator animator;
+    Animator animator; // Animator for controlling animations
 
-    private Vector3 destPoint;
-    private bool walkpointSet;
+    private Vector3 walkPoint; // Random walk point
+    private bool walkPointSet;
+    private float walkPointRange = 10f; // Range for random walk
+
+    [SerializeField] private float walkSpeed = 1.0f, runSpeed = 2.5f; // Movement speeds
+    [SerializeField] private float sightRange, attackRange; // Detection ranges
+    private bool playerInSightRange, playerInAttackRange;
+
     private bool isIdle = true;
-    [SerializeField] private float range, walkSpeed = 1.0f, runSpeed = 2.5f;
+    private float idleTime = 3.0f, walkTime = 5.0f; // Times for state transitions
+    private float timeSinceLastTransition; // Timer tracking
 
-    [SerializeField] float sightRange, attackRange;
-    bool LucasInSight, LucasInAttackRange;
-
-    private float idleTime = 3.0f;
-    private float walkTime = 7.0f;
-    private float timeSinceLastTransition;
     private bool isAttacking = false;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        Lucas = GameObject.Find("Lucas");
         animator = GetComponent<Animator>();
+        agent.isStopped = true;
         timeSinceLastTransition = Time.time;
-        agent.isStopped = true; // Zaustavi agenta na početku
-        animator.SetBool("Walk", false);
-        animator.SetFloat("Run", 0.0f);
     }
 
     void Update()
     {
-        LucasInSight = Physics.CheckSphere(transform.position, sightRange, LucasLayer);
-        LucasInAttackRange = Physics.CheckSphere(transform.position, attackRange, LucasLayer);
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        if (isAttacking)
+        if (!playerInSightRange && !playerInAttackRange)
+            Patrol();
+        else if (playerInSightRange && !playerInAttackRange)
+            ChasePlayer();
+        else if (playerInAttackRange)
+            AttackPlayer();
+
+        ManageAnimations();
+    }
+
+    void Patrol()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
         {
-            // Ako napada, zaustavi agenta
-            agent.isStopped = true;
+            agent.SetDestination(walkPoint);
+            Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+            if (distanceToWalkPoint.magnitude < 1f)
+                walkPointSet = false;
         }
-        else
-        {
-            if (!LucasInSight && !LucasInAttackRange)
-            {
-                Patrol();
-            }
-            else if (LucasInSight && !LucasInAttackRange)
-            {
-                Chase(runSpeed); // Promijenjeno na runSpeed jer je trčanje kada vidi igrača
-            }
-            else if (LucasInSight && LucasInAttackRange)
-            {
-                Attack();
-            }
 
-            ManageAnimations();
+        TransitionBetweenStates();
+    }
+
+    void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    void ChasePlayer()
+    {
+        agent.speed = runSpeed;
+        agent.isStopped = false;
+        agent.SetDestination(Lucas.transform.position);
+
+        // Additional debug to check if the agent is correctly setting the destination
+        Debug.Log("Chasing player, setting destination to: " + Lucas.transform.position);
+
+        // Ensure the agent is not stuck
+        if (agent.isStopped)
+        {
+            Debug.Log("Agent was stopped, restarting agent.");
+            agent.isStopped = false;
+        }
+    }
+
+    void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(Lucas.transform);
+
+        if (!isAttacking)
+        {
+            animator.SetTrigger("Attack");
+            isAttacking = true;
         }
     }
 
     void ManageAnimations()
     {
-        float currentTime = Time.time;
-        if (!LucasInSight)
+        animator.SetBool("Walk", agent.velocity.magnitude > 0.1f && !isAttacking);
+        animator.SetFloat("Run", ((agent.speed == runSpeed) && (agent.velocity.magnitude > 0.1f)) ? 1.0f : 0.0f);
+    }
+
+    void TransitionBetweenStates()
+    {
+        float timePassed = Time.time - timeSinceLastTransition;
+
+        if (isIdle)
         {
-            if (isIdle)
+            if (timePassed > idleTime)
             {
-                if (currentTime - timeSinceLastTransition > idleTime)
-                {
-                    isIdle = false;
-                    timeSinceLastTransition = currentTime;
-                    animator.SetBool("Walk", true);
-                    agent.isStopped = false;
-                    agent.speed = walkSpeed; // Set agent speed to walk speed when walking
-                }
-            }
-            else
-            {
-                if (currentTime - timeSinceLastTransition > walkTime)
-                {
-                    isIdle = true;
-                    timeSinceLastTransition = currentTime;
-                    animator.SetBool("Walk", false);
-                    agent.isStopped = true; // Stop the agent during idle
-                }
+                isIdle = false;
+                agent.isStopped = false;
+                agent.speed = walkSpeed;
+                timeSinceLastTransition = Time.time;
             }
         }
         else
         {
-            timeSinceLastTransition = Time.time;
-            isIdle = false;
-        }
-    }
-
-    void Chase(float speed)
-    {
-        agent.isStopped = false;
-        agent.speed = speed;
-        animator.SetBool("Walk", false);
-        animator.SetFloat("Run", 1.0f);
-        agent.SetDestination(Lucas.transform.position);
-        RotateTowards(Lucas.transform);
-    }
-
-    void Attack()
-    {
-        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
-        {
-            isAttacking = true;
-            animator.SetTrigger("Attack");
-            agent.SetDestination(transform.position);
-        }
-    }
-
-    void OnAttackAnimationEnd() // Ovu funkciju treba pozvati na kraju animacije napada
-    {
-        isAttacking = false;
-        agent.isStopped = false;
-        // Resetiraj animacije
-        animator.ResetTrigger("Attack");
-    }
-
-    void Patrol()
-    {
-        if (!isIdle)
-        {
-            if (!walkpointSet) SearchForDest();
-            if (walkpointSet)
+            if (timePassed > walkTime || agent.remainingDistance < 1f)
             {
-                agent.SetDestination(destPoint);
-                if (Vector3.Distance(transform.position, destPoint) < 10)
-                {
-                    walkpointSet = false;
-                }
+                isIdle = true;
+                agent.isStopped = true;
+                timeSinceLastTransition = Time.time;
             }
         }
     }
 
-    void SearchForDest()
+    public void OnAttackAnimationEnd()
     {
-        float z = Random.Range(-range, range);
-        float x = Random.Range(-range, range);
-        destPoint = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
-        if (Physics.Raycast(destPoint, Vector3.down, groundLayer))
-        {
-            walkpointSet = true;
-        }
-    }
-
-    void RotateTowards(Transform target)
-    {
-        Vector3 direction = (target.position - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        isAttacking = false;
     }
 }
--- Assets/Lucas/Script/Player_Controller.cs
+++ Assets/Lucas/Script/Player_Controller.cs
@@ -1,27 +1,20 @@
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 3.0f;
     public float sprintSpeed = 6.0f;
     public float crouchSpeed = 1.5f;
     public float jumpForce = 10.0f;
-
     private float lastJumpTime = 0;
     private float jumpCooldown = 0.5f;
     public float rotationSpeed = 10.0f;
-
     [SerializeField]
     GameObject Lucas;
-
-
     private Rigidbody rb;
     private Animator animator;
-
     private Vector3 movement = Vector3.zero;
     private bool crouched;
     private bool jump;
-
     void Start()
     {
         rb = Lucas.GetComponent<Rigidbody>();
@@ -29,18 +22,14 @@ public class PlayerController : MonoBehaviour
         jump = false;
         crouched = false;
     }
-
     void Update()
     {
-       
         if (movement.magnitude != 0.0f)
         {
-            
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 animator.SetFloat("Speed", movement.magnitude);
                 animator.SetBool("Walk", false);
-                
             }
             else
             {
@@ -53,14 +42,11 @@ public class PlayerController : MonoBehaviour
             animator.SetFloat("Speed", 0.0f);
             animator.SetBool("Walk", false);
         }
-
-       
         if (Input.GetKeyDown(KeyCode.C))
         {
             crouched = !crouched;
             animator.SetBool("Crouch", crouched);
         }
-
         if (crouched)
         {
             animator.SetFloat("Crouched_Walking", movement.magnitude * crouchSpeed);
@@ -69,20 +55,13 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetFloat("Crouched_Walking", 0.0f);
         }
-
-        
-
-
     bool isGrounded = IsGrounded();
-
-    
     if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !crouched && (Time.time - lastJumpTime > jumpCooldown))
     {
         Vector3 jumpDirection = Vector3.up * jumpForce;
-        
         if (movement.magnitude > 0)
         {
-            jumpDirection += transform.forward * 0.5f;  
+            jumpDirection += transform.forward * 0.5f;
         }
         rb.AddForce(jumpDirection, ForceMode.Impulse);
         animator.SetBool("Jump", true);
@@ -94,51 +73,29 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("Jump", false);
         jump = false;
     }
-   
-        
     }
-
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        
         if (movement.magnitude > 0)
         {
             Quaternion targetRotation = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * rotationSpeed);
-            
         }
-
         if (jump)
             rb.useGravity = false;
         else
             rb.useGravity = true;
     }
-
     private bool IsGrounded()
 {
-    float distanceToGround = 0.5f; 
+    float distanceToGround = 0.5f;
     Vector3 offset = new Vector3(0, 0.1f, 0);
     Vector3 start = transform.position + offset;
     Vector3 direction = Vector3.down;
-
-    
     Debug.DrawLine(start, start + direction * distanceToGround, Color.red);
-
-    
     return Physics.Raycast(start, direction, distanceToGround);
 }
-
 }
-
-
-
-
-
-
-
-    
- 
